54ea276939c2b4680af6befb45891b18b171760f
Jessop Breth <jessopb@gmail.com>
2018-08-03 16:45:46 -0400

    beautifies and disables console logging

--- nightrider.js
+++ nightrider.js
@@ -20,67 +20,62 @@ let NR_state = {
   wait: 50
 }
 
-let NR_update = function(){
-  if(NR_state.phase === "right")
-  {
-    if (NR_state.position === NR_end)
-    {
+let NR_update = function() {
+  if (NR_state.phase === "right") {
+    if (NR_state.position === NR_end) {
       tempstring =
-         NR_base_string.substr(NR_start, NR_state.position) +
-         NR_style_tag_center +
-         NR_base_string[NR_state.position] +
-         NR_style_end_tag
+        NR_base_string.substr(NR_start, NR_state.position) +
+        NR_style_tag_center +
+        NR_base_string[NR_state.position] +
+        NR_style_end_tag
       document.querySelector(".nightrider").innerHTML = tempstring;
 
       NR_state.phase = "left";
-    }
-    else {
-         tempstring =
-            NR_base_string.substr(NR_start, NR_state.position) +
-            NR_style_tag_center +
-            NR_base_string[NR_state.position] +
-            NR_style_end_tag +
-            NR_base_string.substr(NR_state.position + 1)
+    } else {
+      tempstring =
+        NR_base_string.substr(NR_start, NR_state.position) +
+        NR_style_tag_center +
+        NR_base_string[NR_state.position] +
+        NR_style_end_tag +
+        NR_base_string.substr(NR_state.position + 1)
 
-          console.log(tempstring)
-            document.querySelector(".nightrider").innerHTML = tempstring;
+      //console.log(tempstring)
+      document.querySelector(".nightrider").innerHTML = tempstring;
       NR_state.position++
     }
   }
-  if(NR_state.phase === "left"){
-    if (NR_state.position === 0){
+  if (NR_state.phase === "left") {
+    if (NR_state.position === 0) {
       tempstring =
         NR_style_tag_center +
         NR_base_string[NR_state.position] +
         NR_style_end_tag +
-         NR_base_string.substr(NR_start + 1)
+        NR_base_string.substr(NR_start + 1)
 
       document.querySelector(".nightrider").innerHTML = tempstring;
       NR_state.phase = "wait"
-    }
-    else {
+    } else {
       tempstring =
-         NR_base_string.substr(NR_start, NR_state.position) +
-         NR_style_tag_center +
-         NR_base_string[NR_state.position] +
-         NR_style_end_tag +
-         NR_base_string.substr(NR_state.position + 1)
+        NR_base_string.substr(NR_start, NR_state.position) +
+        NR_style_tag_center +
+        NR_base_string[NR_state.position] +
+        NR_style_end_tag +
+        NR_base_string.substr(NR_state.position + 1)
 
-       console.log(tempstring)
-         document.querySelector(".nightrider").innerHTML = tempstring;
-         NR_state.position--
+      //console.log(tempstring)
+      document.querySelector(".nightrider").innerHTML = tempstring;
+      NR_state.position--
     }
   }
-  if(NR_state.phase === "wait"){
-    if(NR_state.wait > 0){
+  if (NR_state.phase === "wait") {
+    if (NR_state.wait > 0) {
       document.querySelector(".nightrider").innerHTML = NR_base_string
       NR_state.wait--
-    }
-    else{
+    } else {
       NR_state.wait = 50
       NR_state.phase = "right"
     }
   }
 }
 
-setInterval("NR_update()",NR_interval_speed);
+setInterval("NR_update()", NR_interval_speed);
